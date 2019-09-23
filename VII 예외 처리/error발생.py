40af16dd93da3ccb2dfc73fe2ae2ee920324646d
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-09-23 14:18:36 +0900

    add error, except

---
+++ VII 예외 처리/error발생.py
@@ -0,0 +1,3 @@
+10/0            #ZeroDivisionError: division by zero (0으로 나눈다)
+spam = "halo"   #TypeError: unsupported operand type(s) for +: 'int' and 'str'
+4 + spam * 3    #NameError: name 'spam' is not defined (spam을 지정해 주지 않음

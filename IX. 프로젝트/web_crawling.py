47c13406dd566aac9e672557e139133cf30ab04c
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-11-25 09:53:50 +0900

    Web Crawling - 연애혁명 예쁜 html 파일 만들기

--- IX. 프로젝트/web_crawling.py
+++ IX. 프로젝트/web_crawling.py
@@ -12,15 +12,17 @@ if __name__ == '__main__':
 
     html = "<html><head><meta charset='utf-8'></head><body>"
     cartoon_titles = soup.find_all("td",attrs={"class":"title"})        # <td class="title">...</td>
-    for cartoon_title in cartoon_titles:                                # cartoon_titles[:2]
-        title = cartoon_title.find("a").text                            # 텍스트 가져오기<a>text</a>
-        link = cartoon_title.find("a").get("href")                      # <a href="">text</a>
+    for cartoon_title in cartoon_titles:        # cartoon_titles[:2]
+        title = cartoon_title.find("a").text        # 텍스트 가져오기<a>text</a>
+        link = cartoon_title.find("a").get("href")          # <a href="">text</a>
         link="https://comic.naver.com"+link
         # print(title)
         # print(link)
-        html+="<a href='{}'>{}</a><br>".format(link,title)                  # <a href='link'>title</a>
+        html+="<a href='{}'>{}</a><br>".format(link,title)      # <a href='link'>title</a>
     html += "</body></html>"
     # print(html)
+    outputSoup = BeautifulSoup(html,"lxml")     # 내가 생성한 html 문자열을 soup 객체로 만들기
+    prettyHtml=str(outputSoup.prettify())
 
     with open("연애혁명.html","w",encoding="utf-8") as f:       # html 파일 만들기
-        f.write(html)
+        f.write(prettyHtml)
